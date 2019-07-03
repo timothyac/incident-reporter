a3f6e2ff7ea3a26b58178a4e60d2d7a962231259
Austin Caron <aus10caron@icloud.com>
2019-07-02 23:42:04 -0400

    Added Router

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="test">
+    <h1>Incident Reporter</h1>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "header"
+};
+</script>
+
+<style scoped>
+nav {
+  background: rgb(78, 78, 78);
+  color: whitesmoke;
+  padding: 1rem;
+}
+</style>
